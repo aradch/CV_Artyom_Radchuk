b65654a4878661810ec0813f1cb12b3049612e38
Artyom Radchuk <radchuk.bil@inbox.ru>
2021-07-10 12:07:47 +0300

    add new files

---
+++ index.js
@@ -0,0 +1,23 @@
+const ruLanguageId = document.getElementById("ru");
+const engLanguageId = document.getElementById("eng");
+const ruLanguageContent = document.getElementById("ruLanguage");
+const engLanguageContent = document.getElementById("engLanguage");
+
+const changeLanguages = (e) => {
+  if (e.currentTarget.className === "changeLanguage active") {
+  } else {
+    e.currentTarget.classList.add("active");
+    if (e.currentTarget.id === "ru") {
+      engLanguageId.classList.remove("active");
+      ruLanguageContent.style.display = "block";
+      engLanguageContent.style.display = "none";
+    } else {
+      ruLanguageId.classList.remove("active");
+      ruLanguageContent.style.display = "none";
+      engLanguageContent.style.display = "block";
+    }
+  }
+};
+
+ruLanguageId.addEventListener("click", changeLanguages);
+engLanguageId.addEventListener("click", changeLanguages);
